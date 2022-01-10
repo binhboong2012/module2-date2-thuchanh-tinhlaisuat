5eea98199f76e160445a7a51714a5d80cfd66aa3
Nguyen Thanh Binh <kubap@MacBook-Pro-cua-KU.local>
2022-01-10 17:07:10 +0700

    done

---
+++ src/com/codegym/Main.java
@@ -0,0 +1,21 @@
+package com.codegym;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhap so tien cho vay: ");
+        double money = scanner.nextDouble();
+        System.out.println("Nhap ti le lai suat hang thang : ");
+        double rate= scanner.nextDouble();
+        System.out.println("Nhap so thang cho vay: ");
+        int month = scanner.nextInt();
+        double totalInterest = 0;
+        for (int i = 0; i < month; i++) {
+            totalInterest += money * (rate/100)/12;
+        }
+        System.out.println("So tien lai sau " + month + "thang la " + totalInterest);
+    }
+}
